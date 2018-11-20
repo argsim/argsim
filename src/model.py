d6d38cef27133c337d13b3a46fbc99b1c7776b9e
ysmiraak <ysmiraak@gmail.com>
2018-11-20 15:13:29 +0100

    update model template

--- src/model.py
+++ src/model.py
@@ -1,7 +1,10 @@
 from util_tf import tf, placeholder
 
 
-def vae(dim_src, dim_tgt, dim_emb, dim_rep, warmup= 1e5, accelerate= 1e-5):
+def vae(src, tgt, len_src, len_tgt, dim_src, dim_tgt, dim_emb, dim_rep, warmup= 1e5, accelerate= 1e-5):
+
+    # src, tgt : int32 (b, t)
+    # len_src, len_tgt : int32 (b,)
 
     # dim_src : src vocab size
     # dim_tgt : tgt vocab size
@@ -11,27 +14,39 @@ def vae(dim_src, dim_tgt, dim_emb, dim_rep, warmup= 1e5, accelerate= 1e-5):
     src_ = placeholder(tf.int32, (None, None), src, 'src_')
     tgt_ = placeholder(tf.int32, (None, None), tgt, 'tgt_')
 
-    gold = todo
+    gold = todo # tgt_ one step into future
 
     with tf.variable_scope('emb_src'):
+        # create a variable for source embedding (dim_src, dim_emb)
+        # look up src tf.gather : (dim_src, dim_emb), (b, s) -> (b, s, dim_emb)
+        # s : src length
         todo
 
     with tf.variable_scope('emb_tgt'):
+        # t : tgt length
+        # (b, t) -> (b, t, dim_emb)
         todo
 
     with tf.variable_scope('encode'):
+        # (b, s, dim_emb) -> (b, dim_emb)
         h = todo
 
     with tf.variable_scope('latent'):
+        # (b, dim_emb) -> (b, dim_rep)
         mu = tf.layers.dense(h, dim_rep, name= 'mu')
         lv = tf.layers.dense(h, dim_rep, name= 'lv')
         with tf.name_scope('z'):
-            h = z = mu + tf.exp(0.5 * lv) * tf.random_normal(shape= tf.shape(lv))
+            z = mu + tf.exp(0.5 * lv) * tf.random_normal(shape= tf.shape(lv))
+        # (b, dim_rep) -> (b, dim_emb)
+        h = tf.layers.dense(z, dim_emb, name= 'proj')
 
     with tf.variable_scope('decode'):
+        # (b, dim_emb) -> (b, t, dim_emb)
+        # remember to ignore the final step
         h = todo
 
     with tf.variable_scope('logit'):
+        # (b, t, dim_emb) -> (b, t, dim_tgt)
         y = todo
 
     with tf.variable_scope('prob'):
@@ -45,14 +60,18 @@ def vae(dim_src, dim_tgt, dim_emb, dim_rep, warmup= 1e5, accelerate= 1e-5):
 
     step = tf.train.get_or_create_global_step()
     with tf.variable_scope('loss'):
+
         with tf.name_scope('loss_gen'):
             loss_gen = tf.reduce_mean(
                 tf.nn.sparse_softmax_cross_entropy_with_logits(labels= gold, logits= y))
+
         with tf.name_scope('loss_kld'):
             loss_kld = 0.5 * tf.reduce_mean(
                 tf.reduce_sum(tf.square(mu) + tf.exp(lv) - lv - 1.0, axis= 1))
+
         with tf.name_scope('balance'):
             balance = tf.nn.sigmoid(accelerate * (tf.to_float(step) - warmup))
+
         loss = balance * loss_kld + loss_gen
 
     up = tf.train.AdamOptimizer().minimize(loss, step)
