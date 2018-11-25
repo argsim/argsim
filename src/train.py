1c196c65225cfc9903b889d6e8b8e1c7c1c15103
ysmiraak <ysmiraak@gmail.com>
2018-11-25 10:10:58 +0100

    adapt training schedule to colab

--- src/train.py
+++ src/train.py
@@ -51,7 +51,7 @@ tgt = pipe(batch, tf.int32, prefetch= 16)
 # build model #
 ###############
 
-model = vae(tgt, dim_emb=256, dim_rep=256)
+model = vae(tgt, dim_emb=256, dim_rep=256, warmup=2e4)
 
 sess = tf.InteractiveSession()
 saver = tf.train.Saver()
