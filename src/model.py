ec59b45b1ab7509a8b1bbd038fcf84bf5f48599b
ysmiraak <ysmiraak@gmail.com>
2018-11-26 23:39:39 +0100

    use cudnn

--- src/model.py
+++ src/model.py
@@ -1,17 +1,17 @@
-from util import identity, partial
+from util import identity, partial, Record
 from util_tf import tf, placeholder
 
 
 # init_kern = tf.variance_scaling_initializer(1.0, 'fan_avg', 'uniform')
 init_kern = tf.variance_scaling_initializer(2.0, 'fan_avg', 'uniform')
 init_bias = tf.zeros_initializer()
-
 layer_aff = partial(tf.layers.dense, kernel_initializer=init_kern, bias_initializer=init_bias)
 layer_act = partial(layer_aff, activation=tf.nn.relu)
-rnn_cell = partial(tf.nn.rnn_cell.GRUCell, activation=tf.nn.relu, kernel_initializer=init_kern, bias_initializer=init_bias)
+scope = partial(tf.variable_scope, reuse=tf.AUTO_REUSE)
 
 
-def vAe(tgt,
+def vAe(mode,
+        tgt=None,
         # model spec
         dim_tgt=8192,
         dim_emb=256,
@@ -19,119 +19,133 @@ def vAe(tgt,
         rnn_layers=2,
         logit_use_embed=False,
         # training spec
-        keep_word=0.5,
-        keep_prob=0.9,
-        warmup=1e4,
+        dropout_rate=0.2,
+        dropout_word=0.5,
         accelerate=1e-4,
+        warmup=1e4,
         eos=1):
-    # tgt : int32 (b, t)  | batchsize, timestep
+    # mode
+    # train: dropout, word dropout, optimizer
+    # valid: without those
+    # infer: autoregressive decoder
+
     # dim_tgt : vocab size
     # dim_emb : model dimension
     # dim_rep : representation dimension
 
-    tgt = placeholder(tf.int32, (None, None), tgt, 'tgt')
-    keep_word = placeholder(tf.float32, (), keep_word, 'keep_word')
-    keep_prob = placeholder(tf.float32, (), keep_prob, 'keep_prob')
+    assert mode in ('train', 'valid', 'infer')
+    self = Record()
 
-    dropout = partial(tf.nn.dropout, keep_prob=keep_prob)
-    rnn_dropout = lambda cell, input_size=dim_emb: tf.nn.rnn_cell.DropoutWrapper(
-        cell=cell,
-        input_keep_prob=keep_prob,
-        output_keep_prob=keep_prob,
-        state_keep_prob=keep_prob,
-        variational_recurrent=True,
-        input_size=input_size,
-        dtype=tf.float32)
+    # int32 (b, t)  | batchsize, timestep
+    tgt = self.tgt = placeholder(tf.int32, (None, None), tgt, 'tgt')
 
-    # disable dropout for now
-    dropout = identity
-    rnn_dropout = lambda cell, input_size=dim_emb: cell
-
-    with tf.variable_scope('input'):
-        with tf.variable_scope('length'):
-            length = tf.reduce_sum(tf.to_int32(tf.not_equal(tgt, eos)), -1)
-            maxlen = tf.reduce_max(length)
-        with tf.variable_scope('mask'):
-            mask = tf.sequence_mask(length, maxlen=maxlen)
-        with tf.variable_scope('gold'):
-            labels = tf.boolean_mask(tgt[:,1:maxlen+1], mask)
-        with tf.variable_scope('tgt_dec'):
-            tgt_dec = tgt[:,:maxlen]
-        with tf.variable_scope('tgt_enc'):
-            tgt_enc = tgt[:,1:maxlen]
-
-    with tf.variable_scope('embed'):
-        # (b, t) -> (b, t, dim_emb)
-        embed_mtrx = tf.get_variable(name="embed_mtrx", shape=[dim_tgt, dim_emb], initializer=init_kern)
-        embed_enc = tf.gather(embed_mtrx, tgt_enc)
-        embed_dec = tf.gather(embed_mtrx, tgt_dec * tf.to_int32(tf.random_uniform(tf.shape(tgt_dec)) < keep_word))
-
-    with tf.variable_scope('encode'):
-        # (b, t, dim_emb) -> (b, dim_emb)
-        _, h_fw, h_bw = tf.contrib.rnn.stack_bidirectional_dynamic_rnn(
-            cells_fw=[rnn_dropout(rnn_cell(dim_emb), dim_emb*(1+i)) for i in range(rnn_layers)],
-            cells_bw=[rnn_dropout(rnn_cell(dim_emb), dim_emb*(1+i)) for i in range(rnn_layers)],
-            inputs=embed_enc,
-            sequence_length=length,
-            dtype=tf.float32)
-        h = tf.concat((h_fw[-1], h_bw[-1]), -1)
-
-    with tf.variable_scope('latent'):
-        # (b, dim_emb) -> (b, dim_rep) -> (b, dim_emb)
-        h = layer_act(h, dim_emb, name='in1')
-        h = layer_act(h, dim_emb, name='in2')
-        mu = layer_aff(h, dim_rep, name='mu')
-        lv = layer_aff(h, dim_rep, name='lv')
-        with tf.name_scope('z'):
-            h = z = mu + tf.exp(0.5 * lv) * tf.random_normal(shape=tf.shape(lv))
-        h = layer_act(h, dim_emb, name='ex1')
-        h = layer_act(h, dim_emb, name='ex2')
-
-    with tf.variable_scope('decode'):
-        # (b, dim_emb) -> (b, t, dim_emb) -> (?, dim_emb)
-        h, _ = tf.nn.dynamic_rnn(rnn_dropout(rnn_cell(dim_emb)), embed_dec, initial_state=h, sequence_length=length)
-        # # keep decoder simple for now
-        # stacked_cells = tf.nn.rnn_cell.MultiRNNCell([rnn_dropout(rnn_cell(dim_emb)) for _ in range(rnn_layers)])
-        # initial_state = tuple(h for _ in range(rnn_layers))
-        # h, _ = tf.nn.dynamic_rnn(stacked_cells, embed_dec, initial_state=initial_state, sequence_length=length)
-        h = tf.boolean_mask(h, mask)
-        h = dropout(layer_act(h, dim_emb, name='out1'))
-        h = dropout(layer_act(h, dim_emb, name='out2'))
-
-    # (?, dim_emb) -> (?, dim_tgt)
-    if logit_use_embed:
-        logits = tf.matmul(h, embed_mtrx, transpose_b=True, name='logit')
+    if 'train' == mode:
+        dropout = partial(tf.nn.dropout, keep_prob=1.0-dropout_rate)
     else:
-        logits = layer_aff(h, dim_tgt, name='logit')
+        dropout = identity
 
-    with tf.variable_scope('prob'):
-        prob = tf.nn.softmax(logits)
+    with scope('input'):
+        tgt = tf.transpose(tgt) # time major order
+        with scope('length'):
+            length = tf.reduce_sum(tf.to_int32(tf.not_equal(tgt, eos)), 0)
+            maxlen = tf.reduce_max(length)
+        with scope('mask'):
+            mask = tf.transpose(tf.sequence_mask(length, maxlen=maxlen))
+        with scope('gold'):
+            labels = tf.boolean_mask(tgt[1:maxlen+1], mask)
+        with scope('tgt_dec'):
+            tgt_dec = tgt[0:maxlen]
+        with scope('tgt_enc'):
+            tgt_enc = tgt[1:maxlen]
 
-    with tf.variable_scope('pred'):
-        pred = tf.argmax(logits, -1, output_type=tf.int32)
+    with scope('embed'):
+        # (t, b) -> (t, b, dim_emb)
+        embedding = tf.get_variable(
+            initializer=tf.variance_scaling_initializer(1.0, 'fan_out', 'uniform'),
+            shape=(dim_tgt, dim_emb),
+            name="embedding")
+        if 'train' == mode:
+            with scope('word_dropout'):
+                # pad to never drop bos
+                tgt_dec *= tf.pad(
+                    tf.to_int32(dropout_word <= tf.random_uniform(tf.shape(tgt_enc))),
+                    paddings=((1,0),(0,0)),
+                    constant_values=1)
+        with scope('embed_dec'): embed_dec = dropout(tf.gather(embedding, tgt_dec))
+        with scope('embed_enc'): embed_enc = dropout(tf.gather(embedding, tgt_enc))
 
-    with tf.variable_scope('acc'):
-        acc = tf.reduce_mean(tf.to_float(tf.equal(labels, pred)))
+    with scope('encode'):
+        # (t, b, dim_emb) -> (b, dim_emb)
+        # bidirectional won't work correctly without length mask which cudnn doesn't take;
+        # stick to unidirectional for now;
+        # maybe combine this with a reverse run, or add attention
+        h, _ = tf.contrib.cudnn_rnn.CudnnGRU(
+            num_layers=rnn_layers,
+            num_units=dim_emb,
+            direction='unidirectional',
+            dropout=dropout_rate if 'train' == mode else 0.0
+        )(embed_enc)
+        # extract the states from the outputs at the steps corresponding to the lengths;
+        # since the encoder does not take bos, and the indices are one less of the lengths,
+        # in total it must be offset by 2
+        with scope('cata'):
+            h = tf.gather_nd(h, tf.stack((length-2, tf.range(tf.size(length), dtype=tf.int32)), axis=-1))
 
-    with tf.variable_scope('loss'):
-        step = tf.train.get_or_create_global_step()
-        with tf.name_scope('loss_gen'):
-            loss_gen = tf.reduce_mean(
+    with scope('latent'):
+        # (b, dim_emb) -> (b, dim_rep) -> (b, dim_emb)
+        # no dropout here since we do not want distributed representation
+        with scope('in1'): h = layer_act(h, dim_emb)
+        with scope('in2'): h = layer_act(h, dim_emb)
+        with scope('mu'): mu = self.mu = layer_aff(h, dim_rep)
+        with scope('lv'): lv = self.lv = layer_aff(h, dim_rep)
+        with scope('z'): h = self.z = mu + tf.exp(0.5 * lv) * tf.random_normal(shape=tf.shape(lv))
+        with scope('ex1'): h = layer_act(h, dim_emb)
+        with scope('ex2'): h = layer_act(h, dim_emb)
+
+    with scope('decode'):
+        # (b, dim_emb) -> (t, b, dim_emb) -> (?, dim_emb)
+        h, _ = tf.contrib.cudnn_rnn.CudnnGRU(
+            num_layers=1, # keep decoder simple for now
+            num_units=dim_emb,
+            direction='unidirectional',
+            dropout=dropout_rate if 'train' == mode else 0.0
+        )(embed_dec, initial_state=(tf.expand_dims(h, 0),))
+        h = tf.boolean_mask(h, mask)
+        with scope('out1'): h = dropout(layer_act(h, dim_emb))
+        with scope('out2'): h = dropout(layer_act(h, dim_emb))
+
+    with scope('logit'):
+        # (?, dim_emb) -> (?, dim_tgt)
+        if logit_use_embed:
+            logits = self.logits = tf.matmul(h, embed_mtrx, transpose_b=True)
+        else:
+            logits = self.logits = layer_aff(h, dim_tgt)
+
+    with scope('prob'):
+        prob = self.prob = tf.nn.softmax(logits)
+
+    with scope('pred'):
+        pred = self.pred = tf.argmax(logits, -1, output_type=tf.int32)
+
+    with scope('acc'):
+        acc = self.acc = tf.reduce_mean(tf.to_float(tf.equal(labels, pred)))
+
+    step = self.step = tf.train.get_or_create_global_step()
+    with scope('loss'):
+        with scope('loss_gen'):
+            loss_gen = self.loss_gen = tf.reduce_mean(
                 tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits))
-        with tf.name_scope('loss_kld'):
-            loss_kld = 0.5 * tf.reduce_mean(tf.square(mu) + tf.exp(lv) - lv - 1.0)
-        with tf.name_scope('balance'):
-            balance = tf.nn.relu(tf.tanh(accelerate * (tf.to_float(step) - warmup)))
-        loss = balance * loss_kld + loss_gen
-    train_step = tf.train.AdamOptimizer().minimize(loss, step)
+        with scope('loss_kld'):
+            loss_kld = self.loss_kld = 0.5 * tf.reduce_mean(tf.square(mu) + tf.exp(lv) - lv - 1.0)
+        with scope('balance'):
+            balance = self.balance = tf.nn.relu(tf.tanh(accelerate * (tf.to_float(step) - warmup)))
+        loss = self.loss = balance * loss_kld + loss_gen
 
-    return dict(
-        tgt=tgt, keep_word=keep_word, keep_prob=keep_prob,
-        mu=mu, lv=lv,
-        logits=logits, prob=prob, pred=pred, acc=acc,
-        step=step,
-        loss=loss,
-        loss_gen=loss_gen,
-        loss_kld=loss_kld,
-        balance=balance,
-        train_step=train_step)
+    if 'train' == mode:
+        with scope('train'):
+            train_step = self.train_step = tf.train.AdamOptimizer().minimize(loss, step)
+
+    return self
+
+# - autoregressive loop for infer mode
+# - functions for running model
--- src/train.py
+++ src/train.py
@@ -4,20 +4,20 @@ import sys
 if 1 == len(sys.argv): ckpt = None
 if 2 == len(sys.argv): ckpt = sys.argv[1]
 if 3 <= len(sys.argv): sys.exit("wrong args")
-trial = "master"
+trial = "cudnn"
 
 path_vocab = "../trial/data/vocab.model"
 path_train = "../trial/data/train.txt"
 path_valid = "../trial/data/valid.npy"
 path_ckpt = "../trial/ckpt"
-path_log = "../trial/log"
-# path_log = "/cache/tensorboard-logdir/argsim"
+path_log = "../trial/log" # on colab
+# path_log = "/cache/tensorboard-logdir/argsim" # on jarvis
 
 seed = 0
 batch_train = 256
 batch_valid = 1024
 # batch_train = 64
-# batch_valid = 128
+# batch_valid = 256
 
 from model import vAe as vae
 from tqdm import tqdm
@@ -45,13 +45,25 @@ def batch(size= batch_train, path= path_train, vocab= vocab, seed= seed, eos= 1)
         if 1 < len(s) <= 256:
             bat.append(s)
 
-tgt = pipe(batch, tf.int32, prefetch= 16)
-
 ###############
 # build model #
 ###############
 
-model = vae(tgt, dim_emb=256, dim_rep=256)
+model_valid = vae('valid')
+
+# # for profiling
+# from util_tf import profile
+# with tf.Session() as sess:
+#     tf.global_variables_initializer().run()
+#     with tf.summary.FileWriter(pform(path_log, trial), sess.graph) as wtr:
+#         profile(sess, wtr, model_valid['loss'], feed_dict= {model_valid['tgt']: valid[:32]})
+
+tgt = pipe(batch, tf.int32, prefetch= 16)
+model_train = vae('train', tgt=tgt)
+
+############
+# training #
+############
 
 sess = tf.InteractiveSession()
 saver = tf.train.Saver()
@@ -60,27 +72,17 @@ if ckpt:
 else:
     tf.global_variables_initializer().run()
 
-# # for profiling
-# from util_tf import profile
-# with tf.summary.FileWriter(pform(path_log, trial), sess.graph) as wtr:
-#     profile(sess, wtr, model['loss'], feed_dict= {model['tgt']: valid[:32]})
-
-############
-# training #
-############
-
-summary = tf.summary.merge(
-    (tf.summary.scalar('step_acc',      model['acc']),
-     tf.summary.scalar('step_loss',     model['loss']),
-     tf.summary.scalar('step_loss_gen', model['loss_gen']),
-     tf.summary.scalar('step_loss_kld', model['loss_kld'])))
-
 wtr = tf.summary.FileWriter(pform(path_log, trial))
+summary = tf.summary.merge(
+    (tf.summary.scalar('step_acc',      model_valid['acc']),
+     tf.summary.scalar('step_loss',     model_valid['loss']),
+     tf.summary.scalar('step_loss_gen', model_valid['loss_gen']),
+     tf.summary.scalar('step_loss_kld', model_valid['loss_kld'])))
 
-def summ(step):
-    fetches = model['acc'], model['loss'], model['loss_gen'], model['loss_kld']
+def summ(step, model=model_valid):
+    fetches = model.acc, model.loss, model.loss_gen, model.loss_kld
     results = map(np.mean, zip(*(
-        sess.run(fetches, {model['tgt']: valid[i:j]})
+        sess.run(fetches, {model.tgt: valid[i:j]})
         for i, j in partition(len(valid), batch_valid, discard= False))))
     wtr.add_summary(sess.run(summary, dict(zip(fetches, results))), step)
 
@@ -88,7 +90,7 @@ def summ(step):
 for epoch in range(2): # train for 2 epochs at a time
     for _ in range(50):
         for _ in tqdm(range(100), ncols= 70):
-            sess.run(model['train_step'])
-        step = sess.run(model['step'])
+            sess.run(model_train.train_step)
+        step = sess.run(model_train.step)
         summ(step)
     saver.save(sess, pform(path_ckpt, trial, step // 5000), write_meta_graph= False)
