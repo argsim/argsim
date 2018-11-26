c60914b9a365de00a64ff1aa6d46fa8b0ecd9e96
Jan <jan.milde@hotmail.de>
2018-11-26 21:01:53 +0100

    Cleaned Test Data Processing Code

--- src/prep_test_data.py
+++ src/prep_test_data.py
@@ -1,36 +1,41 @@
 import os
 import numpy as np
-from collections import Counter
-from operator import itemgetter
-import heapq
 
-def least_common(array, to_find=None):
-    counter = Counter(array)
-    if to_find is None:
-        return sorted(counter.items(), key=itemgetter(1), reverse=False)
-    return heapq.nsmallest(to_find, counter.items(), key=itemgetter(1))
+############################################
+### Processes and saves the Test Dataset ###
+############################################
 
+datadir = '../data/reason/reason'
+labels, arguments,idx2lbl,lbl2idx = [], [], {}, {}
 
-datadir = '/home/jan/Documents/uni/Argumentation_Mining/dataset/data'
-
-
-labels, arguments = [], []
-
-for subdir, dirs, files in os.walk(datadir):
+for dirs, subdirs, files in os.walk(datadir):
+    # don't look into unwanted folders
+    if 'labels' and 'folds' in subdirs:
+        subdirs.remove('labels')
+        subdirs.remove('folds')
+    # skip readme file
     for file in files:
-        #print(os.path.join(subdir, file))
-        text = open(os.path.join(subdir, file),encoding = "Windows-1252").read()
-        text = text.split("\n")
-        for idx, sentence in enumerate(text):
-            if sentence[:7] == "Label##":
-                count = 1
-                try:
-                    while text[idx+count][:7] != "Label##":
-                        labels.append(text[idx][7:])
-                        arguments.append(text[idx+count][6:])
-                        count += 1
-                except IndexError:
-                    continue
+        if file == "readme.txt":
+            continue
+        else:
+            text = open(os.path.join(dirs, file),encoding = "Windows-1252").read()
+            text = text.split("\n")
+            for idx, sentence in enumerate(text):
+                if sentence[:7] == "Label##":
+                    count = 1
+                    try:
+                        while text[idx+count][:7] != "Label##":
+                            labels.append(text[idx][7:])
+                            arguments.append(text[idx+count][6:])
+                            count += 1
+                    except IndexError:
+                        continue
 
-counts = Counter(labels)
-least_common(counts)
+# Creates lookup dictionaries
+for idx,lbl in enumerate(set(labels)):
+    idx2lbl[idx] = lbl
+    lbl2idx[lbl] = idx
+num_lbls = [lbl2idx[lbl] for lbl in labels]
+
+data = np.asarray([arguments, num_lbls, idx2lbl, lbl2idx])
+np.save('../data/test_data.npy', data)
