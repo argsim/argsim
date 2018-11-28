e2b64b8c2a7817a72480e605ca0de53e3ee0e085
ysmiraak <ysmiraak@gmail.com>
2018-11-28 18:47:22 +0100

    bidirectional attention

--- src/model.py
+++ src/model.py
@@ -4,6 +4,7 @@ from util_tf import tf, placeholder
 
 
 init_bias = tf.zeros_initializer()
+init_embd = tf.random_uniform_initializer(-0.5, 0.5)
 init_kern = tf.variance_scaling_initializer(1.0, 'fan_avg', 'uniform')
 init_relu = tf.variance_scaling_initializer(2.0, 'fan_avg', 'uniform')
 
@@ -12,6 +13,29 @@ layer_act = partial(tf.layers.dense, kernel_initializer=init_relu, bias_initiali
 layer_rnn = partial(tf.contrib.cudnn_rnn.CudnnGRU, kernel_initializer=init_kern, bias_initializer=init_bias)
 
 scope = partial(tf.variable_scope, reuse=tf.AUTO_REUSE)
+
+
+def attention(query, value, mask, dim, transform=False):
+    """computes scaled dot-product attention
+
+    query : tensor f32 (b, s, d_q)
+    value : tensor f32 (b, t, d_v)
+     mask : tensor f32 (b, s, t)
+         -> tensor f32 (b, s, dim)
+
+    transform may be omitted when `dim == d_q == d_v`
+
+    """
+    k, v, q = value, value, query
+    if transform:
+        k = layer_aff(k, dim, name='k') # btd key
+        v = layer_aff(v, dim, name='v') # btd value
+        q = layer_aff(q, dim, name='q') # bsd query
+    a = tf.matmul(q, k, transpose_b=True) # weight: bst <- bsd @ (bdt <- btd)
+    a *= dim ** -0.5 # scale by sqrt dim
+    a += mask # mask
+    a = tf.nn.softmax(a) # normalize
+    return a @ v # attend: bsd <- bst @ btd
 
 
 def vAe(mode,
@@ -47,16 +71,21 @@ def vAe(mode,
         max_len = tf.reduce_max(len_seq)
         # trims extra bos to make sure the lengths are right
         tgt, not_eos = tgt[:max_len], not_eos[:max_len]
+        # tgt reversed and having eos replaced with bos
+        gtg = tf.reverse_sequence(tgt, len_seq, seq_axis=0, batch_axis=1)
+        gtg = tf.where(not_eos, gtg, tf.fill(tf.shape(gtg), bos))
 
     with scope('mask'):
         mask_dec = tf.pad(not_eos, ((1,0),(0,0)), constant_values=True)
         mask_enc = tf.pad(not_eos, ((0,1),(0,0)), constant_values=False)
 
     with scope('embed'): # (t, b) -> (t, b, dim_emb)
-        embedding = tf.get_variable('embedding', (dim_tgt, dim_emb), initializer=tf.random_uniform_initializer(-0.5, 0.5))
-        with scope('embed_enc'): # pads one eos for the attention query
+        embedding = tf.get_variable('embedding', (dim_tgt, dim_emb), initializer=init_embd)
+        with scope('embed_enc'): # pads one eos or bos for the attention query
             tgt_enc = tf.pad(tgt, paddings=((0,1),(0,0)), constant_values=eos)
+            gtg_enc = tf.pad(gtg, paddings=((0,1),(0,0)), constant_values=bos)
             embed_enc = tf.gather(embedding, tgt_enc)
+            embed_gtg = tf.gather(embedding, gtg_enc)
         with scope('embed_dec'): # pads one bos for the initial step
             if 'train' == mode:
                 with scope('drop_word'):
@@ -83,54 +112,47 @@ def vAe(mode,
     with scope('encode'): # (t, b, dim_emb) -> (b, dim_emb)
         # bidirectional won't work correctly without length mask which cudnn doesn't take;
         # stick to unidirectional for now;
-        # maybe combine this with a reverse run
-        tbd, _ = layer_rnn(rnn_layers, dim_emb)(embed_enc)
+        tgt, _ = layer_rnn(rnn_layers, dim_emb, name='rnn_fwd')(embed_enc) # tbd
+        gtg, _ = layer_rnn(rnn_layers, dim_emb, name='rnn_bwd')(embed_gtg) # tbd
         with scope('cata'):
-            # extract the final states which are the outputs from the first eos steps
-            h = bd = tf.gather_nd(tbd, tf.stack((len_seq, tf.range(tf.size(len_seq), dtype=tf.int32)), axis=1))
+            # extract the final states which are the outputs from the first padding steps
+            idx = tf.stack((len_seq, tf.range(tf.size(len_seq), dtype=tf.int32)), axis=1) # b2
+            # forward is backward
+            fwd = tf.gather_nd(gtg, idx) # bd <- tbd, b2
+            bwd = tf.gather_nd(tgt, idx) # bd <- tbd, b2
             if attentive:
-                # scaled dot-product attention;
-                # the values are the outputs from all non-eos steps;
-                # the queries are the final states
-                v = tf.transpose(tbd, (1,0,2)) # values: btd <- tbd
-                q = tf.expand_dims(bd, axis=1) # queries: b1d <- bd
-                k = v
-                if False: # key value attention
-                    k = layer_aff(v, dim_rep, name='k') # btd key
-                    v = layer_aff(v, dim_rep, name='v') # btd value
-                    q = layer_aff(q, dim_rep, name='q') # b1d query
-                a = tf.matmul(q, k, transpose_b=True) # weights: b1t <- b1d @ (bdt <- btd)
-                a *= dim_emb ** -0.5 # scale by sqrt dim_emb
-                a += tf.log(tf.to_float(tf.expand_dims(tf.transpose(mask_enc), 1))) # mask eos steps
-                a = tf.nn.softmax(a) # normalize
-                h = tf.squeeze(a @ v, 1) # attend: bd <- b1d <- b1t @ btd
-                # h += bd # residual connection
+                # the values are the outputs from all non-padding steps;
+                # the queries are the final states;
+                # padding mask: b1t <- tb <- bt
+                msk = tf.log(tf.to_float(tf.expand_dims(tf.transpose(mask_enc), axis=1)))
+                # query: b1d <- bd
+                fwd = tf.expand_dims(fwd, axis=1)
+                bwd = tf.expand_dims(bwd, axis=1)
+                # value: btd <- tbd
+                tgt = tf.transpose(tgt, (1,0,2))
+                gtg = tf.transpose(gtg, (1,0,2))
+                # attend: b1d <- b1d, btd, b1t
+                with scope('fwd'): fwd = attention(fwd, tgt, msk, dim_emb)
+                with scope('bwd'): bwd = attention(bwd, gtg, msk, dim_emb)
+                # bd <- b1d
+                fwd = tf.squeeze(fwd, axis=1)
+                bwd = tf.squeeze(bwd, axis=1)
 
-    with scope('latent'): # (b, dim_emb) -> (b, dim_rep) -> (b, dim_emb)
-        # h = layer_act(h, dim_emb, name='in')
-        # h_mu = layer_act(h, dim_emb, name='in_mu')
-        # h_lv = layer_act(h, dim_emb, name='in_lv')
-        h = layer_aff(h, dim_emb, name='in1')
-        h = layer_aff(h, dim_emb, name='in2')
+    with scope('latent'): # (b, dim_emb), (b, dim_emb) -> (b, dim_rep) -> (b, dim_emb)
+        fwd = layer_aff(fwd, dim_emb, name='in_fwd')
+        bwd = layer_aff(bwd, dim_emb, name='in_bwd')
+        h = fwd + bwd
         mu = self.mu = layer_aff(h, dim_rep, name='mu')
         lv = self.lv = layer_aff(h, dim_rep, name='lv')
         with scope('z'): h = self.z = mu + tf.exp(0.5 * lv) * tf.random_normal(shape=tf.shape(lv))
-        # h = layer_act(h, dim_emb, name='ex')
-        h = layer_aff(h, dim_emb, name='ex1')
-        h = layer_aff(h, dim_emb, name='ex2')
-        # h = layer_act(h, dim_emb, name='ex1')
-        # h = layer_act(h, dim_emb, name='ex2')
+        h = layer_aff(h, dim_emb, name='ex')
 
     with scope('decode'): # (b, dim_emb) -> (t, b, dim_emb) -> (?, dim_emb)
         self.tgt_dec = tgt_dec
-        h = self.state_in = tf.expand_dims(h, 0)
-        h, _ = _, (self.state_ex,) = layer_rnn(1, dim_emb)(embed_dec, initial_state=(h,))
-        # keep the decoder simple for now by using more dense instead of rnn layers
+        h = self.state_in = tf.expand_dims(h, axis=0)
+        h, _ = _, (self.state_ex,) = layer_rnn(1, dim_emb, name='rnn')(embed_dec, initial_state=(h,))
+        # keep the decoder simple for now, just 1 rnn layer
         if 'infer' != mode: h = tf.boolean_mask(h, mask_dec)
-        # h = layer_act(h, dim_emb, name='out1')
-        # h = layer_act(h, dim_emb, name='out2')
-        h = layer_aff(h, dim_emb, name='out1')
-        h = layer_aff(h, dim_emb, name='out2')
 
     with scope('logits'): # (?, dim_emb) -> (?, dim_tgt)
         if logit_use_embed:
