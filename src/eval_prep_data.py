ddb87633dd1e9685666a0bd2d02e8cef100c5a24
ysmiraak <ysmiraak@gmail.com>
2018-12-16 02:54:46 +0100

    change eval_pred_data to produce instances in deterministic order

--- src/eval_prep_data.py
+++ src/eval_prep_data.py
@@ -15,35 +15,28 @@ for filename in os.listdir("{}/folds".format(datadir)):
         for line in load_txt("{}/folds/{}".format(datadir, filename)):
             file2fold[line.split()[0]] = fold
 
-labels, arguments = [], []
-for dirs, subdirs, files in os.walk(datadir):
-    topic = dirs.split("/")[-1]
-    # don't look into unwanted folders
-    if 'labels' and 'folds' in subdirs:
-        subdirs.remove('labels')
-        subdirs.remove('folds')
-    for file in files:
-        # skip readme file
-        if file == "readme.txt":
-            continue
-        else:
-            fold = file2fold[file.split(".")[0]]
-            text = list(load_txt(os.path.join(dirs, file), encoding="Windows-1252"))
-            for idx, sentence in enumerate(text):
-                if sentence[:7] == "Label##":
-                    # the other class is usually Other but in abortion it's other
-                    stance, reason = sentence[7:].lower().split("-")
-                    if "other" == reason: continue #exclude OTHER class
-                    label = "{}-{}-{}-{}".format(topic, fold, stance, reason)
-                    count = 1
-                    try:
-                        while text[idx+count][:6] == "Line##":
-                            labels.append(label)
-                            arguments.append(text[idx+count][6:])
-                            count += 1
-                    except IndexError:
-                        continue
+folds, labels, arguments = [], [], []
+for topic in 'abortion', 'gayRights', 'marijuana', 'obama':
+    dirname = "{}/{}".format(datadir, topic)
+    for filename in sorted(os.listdir(dirname)):
+        fold = int(file2fold[filename.split(".")[0]]) - 1
+        text = list(load_txt(os.path.join(dirname, filename), encoding="Windows-1252"))
+        for idx, sentence in enumerate(text):
+            if sentence[:7] == "Label##":
+                stance, reason = sentence[7:].lower().split("-")
+                # if "other" == reason: continue #exclude OTHER class
+                label = "{}-{}-{}".format(topic, stance, reason)
+                count = 1
+                try:
+                    while text[idx+count][:6] == "Line##":
+                        folds.append(fold)
+                        labels.append(label)
+                        arguments.append(text[idx+count][6:])
+                        count += 1
+                except IndexError:
+                    continue
 
 # save the data
 save_txt("../data/test_data.txt", arguments)
 np.save("../data/test_labels.npy", np.asarray(labels))
+np.save("../data/test_folds.npy", np.asarray(folds))
