fix summary averaging

src/model.py
@@ -165,20 +165,17 @@ def vAe(mode,
 
     if 'infer' != mode:
         labels = tf.boolean_mask(gold, msk_dec, name='labels')
-        with scope('errt'): errt = self.errt = tf.reduce_mean(tf.to_float(tf.not_equal(labels, pred)))
-
+        with scope('errt'):
+            errt_samp = self.errt_samp = tf.to_float(tf.not_equal(labels, pred))
+            errt      = self.errt      = tf.reduce_mean(errt_samp)
         with scope('loss'):
             with scope('loss_gen'):
-                loss_gen = self.loss_gen = tf.reduce_mean(
-                    tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits))
+                loss_gen_samp = self.loss_gen_samp = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits)
+                loss_gen      = self.loss_gen      = tf.reduce_mean(loss_gen_samp)
             with scope('loss_kld'):
-                loss_kld = tf.reduce_mean(tf.square(mu) + tf.exp(lv) - lv - 1.0)
-                if 'train' == mode:
-                    loss_kld *= 0.5 * rate_anneal
-                else:
-                    loss_kld *= 0.5
-                self.loss_kld = loss_kld
-            loss = self.loss = loss_kld + loss_gen
+                loss_kid_samp = self.loss_kid_samp = 0.5 * (tf.square(mu) + tf.exp(lv) - lv - 1.0)
+                loss_kld      = self.loss_kld      = tf.reduce_mean(loss_kid_samp)
+            loss = self.loss = rate_anneal * loss_kld + loss_gen
 
     if 'train' == mode:
         with scope('train'):

src/train.py
@@ -28,7 +28,7 @@ os.environ['CUDA_VISIBLE_DEVICES'] = A.gpu
 # preparation #
 ###############
 
-from util import Record
+from util import Record, comp
 from model import vAe as vae
 from tqdm import tqdm
 from util_io import pform, load_txt, load_json
@@ -94,16 +94,18 @@ else:
 
 wtr = tf.summary.FileWriter(pform(P.log, A.trial))
 summary = tf.summary.merge(
-    (tf.summary.scalar('step_errt',     model_valid['errt']),
-     tf.summary.scalar('step_loss_gen', model_valid['loss_gen']),
-     tf.summary.scalar('step_loss_kld', model_valid['loss_kld'])))
+    (tf.summary.scalar('step_errt'    , model_valid.errt    ),
+     tf.summary.scalar('step_loss_gen', model_valid.loss_gen),
+     tf.summary.scalar('step_loss_kld', model_valid.loss_kld)))
 
 def summ(step, model=model_valid):
-    fetches = model.errt, model.loss_gen, model.loss_kld
-    results = map(np.mean, zip(*(
-        sess.run(fetches, {model.tgt: valid[i:j]})
-        for i, j in partition(len(valid), T.batch_valid, discard= False))))
-    wtr.add_summary(sess.run(summary, dict(zip(fetches, results))), step)
+    wtr.add_summary(
+        sess.run(summary, dict(zip(
+            (model.errt, model.loss_gen, model.loss_kld),
+            map(comp(np.mean, np.concatenate),
+                zip(*(sess.run((model.errt_samp, model.loss_gen_samp, model.loss_kld_samp), {model.tgt: valid[i:j]})
+                      for i, j in partition(len(valid), T.batch_valid, discard= False))))))),
+        step)
 
 for _ in range(A.rounds):
     for _ in range(100):