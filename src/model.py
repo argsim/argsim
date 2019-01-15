14547c6a099834a4ecf17ba1e0dfd99a1dd69c94
ysmiraak <ysmiraak@gmail.com>
2019-01-15 16:05:53 +0100

    revert accelerate, use a less extreme lr decay

--- src/model.py
+++ src/model.py
@@ -76,7 +76,7 @@ def vAe(mode,
         rate = accelerate * tf.to_float(step)
         rate_keepwd = self.rate_keepwd = tf.sigmoid(rate)
         rate_anneal = self.rate_anneal = tf.tanh(rate)
-        rate_update = self.rate_update = tf.reciprocal(1.0 + rate) * 1e-3
+        rate_update = self.rate_update = tf.rsqrt(rate + 1.0) * 1e-3
 
     with scope('src'):
         src = self.src = placeholder(tf.int32, (None, None), src, 'src')
